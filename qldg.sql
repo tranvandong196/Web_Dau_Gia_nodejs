e79875a54390ae27c66a7233b8d4c0b132c952c2
nguyenquocduy261296 <duy371704@gmail.com>
2017-06-23 13:30:07 +0700

    csdl update

--- qldg.sql
+++ qldg.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : MySQL
+Source Server         : Mysql
 Source Server Version : 50717
 Source Host           : localhost:3306
 Source Database       : qldg
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-06-22 11:19:53
+Date: 2017-06-23 10:22:48
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -33,6 +33,10 @@ CREATE TABLE `auctions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
+-- Records of auctions
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for categories
 -- ----------------------------
 DROP TABLE IF EXISTS `categories`;
@@ -40,7 +44,17 @@ CREATE TABLE `categories` (
   `CatID` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `CatName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`CatID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of categories
+-- ----------------------------
+INSERT INTO `categories` VALUES ('1', 'Quần áo');
+INSERT INTO `categories` VALUES ('2', 'Giày dép');
+INSERT INTO `categories` VALUES ('3', 'Điện thoại');
+INSERT INTO `categories` VALUES ('4', 'Máy tính');
+INSERT INTO `categories` VALUES ('5', 'Đồng hồ');
+INSERT INTO `categories` VALUES ('6', 'Điện  máy gia đình');
 
 -- ----------------------------
 -- Table structure for favorites
@@ -58,6 +72,10 @@ CREATE TABLE `favorites` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
+-- Records of favorites
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for products
 -- ----------------------------
 DROP TABLE IF EXISTS `products`;
@@ -70,10 +88,10 @@ CREATE TABLE `products` (
   `CatID` int(11) unsigned NOT NULL,
   `Quantity` int(11) unsigned NOT NULL,
   `PriceToBuy` int(11) DEFAULT NULL,
-  `UserID` int(11) unsigned NOT NULL,
+  `UserID` int(11) unsigned DEFAULT NULL,
   `HandleID` int(11) unsigned DEFAULT NULL,
-  `TimeUp` datetime NOT NULL,
-  `TimeDown` datetime NOT NULL,
+  `TimeUp` datetime DEFAULT NULL,
+  `TimeDown` datetime DEFAULT NULL,
   PRIMARY KEY (`ProID`),
   KEY `fk_products_categories` (`CatID`),
   KEY `fk_products_users1` (`UserID`),
@@ -81,7 +99,59 @@ CREATE TABLE `products` (
   CONSTRAINT `fk_products_categories` FOREIGN KEY (`CatID`) REFERENCES `categories` (`CatID`),
   CONSTRAINT `fk_products_users` FOREIGN KEY (`HandleID`) REFERENCES `users` (`ID`),
   CONSTRAINT `fk_products_users1` FOREIGN KEY (`UserID`) REFERENCES `users` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of products
+-- ----------------------------
+INSERT INTO `products` VALUES ('1', 'Sơ mi nam cao cấp phong cách-SM0013500', 'Vải giãn nhung HQ,mịn,co giãn thoải mái,dày dặn,mịn màng,cuộn sườn,đẹp hết nấc', 'Vải giãn nhung HQ,mịn,co giãn thoải mái,dày dặn,mịn màng,cuộn sườn,đẹp hết nấc. Size M dưới 60-62kg,Size L dưới 65-67kg, Size XL dưới 70-72kg tuỳ chiều cao và cân nặng ', '370000', '1', '5', '400000', null, null, null, null);
+INSERT INTO `products` VALUES ('2', 'Sơ mi nam tay ngắn thời trang-ASM01295210', 'Cotton xí nghiệp,mềm,mượt', 'Chất liệu: Cotton xí nghiệp,mềm,mượt\r\nSize: Size M dưới 60kg,Size L dưới 65kg, Size XL dưới 70kg tuỳ chiều cao và cân nặng \r\nMàu sắc: 3 màu y hình', '295000', '1', '3', '350000', null, null, null, null);
+INSERT INTO `products` VALUES ('3', 'Áo thun nam thời trang-AT01260210', 'Thun cotton 4 chiều,dày,mượt', 'Chất liệu: Thun cotton 4 chiều,dày,mượt\r\nSize: M,L,XL\r\nMàu sắc: Trắng, đen\r\nHàng shop', '260000', '1', '4', '30000', null, null, null, null);
+INSERT INTO `products` VALUES ('4', 'Áo thun nam thời trang-ATN009000', 'dày dặn,không xù lông,không rút,thấm hút mồ hôi,bo dệt,đẹp từng centimet', 'Chất liệu: Thun 100% cotton co giãn 4 chiều,mượt,dày dặn,không xù lông,không rút,thấm hút mồ hôi,bo dệt,đẹp từng centimet\r\nSize: M,L,XL \r\nMàu sắc: 2 màu như hình', '290000', '1', '3', '350000', null, null, null, null);
+INSERT INTO `products` VALUES ('5', 'Quần jeans nam orginaility', 'thoải mái, êm', 'null', '310000', '1', '2', '400000', null, null, null, null);
+INSERT INTO `products` VALUES ('6', ' \r\nQuần dài kaki nam cá tính', 'thoải mái, êm', 'null', '240000', '1', '6', '300000', null, null, null, null);
+INSERT INTO `products` VALUES ('7', 'Quần sort nam-QS0011500', 'thoải mái, êm', 'Chất liệu:Cotton nhập \r\nSize: 29,30,31,32\r\nMàu sắc: 2 màu y hình', '330000', '1', '5', '400000', null, null, null, null);
+INSERT INTO `products` VALUES ('8', ' \r\nQuần Short nam Polo Ralph Lauren', 'thoải mái, êm', 'Hàng chính hãng nhập khẩu từ Mỹ\r\nGiao hàng miễn phí', '1775000', '1', '3', '2000000', null, null, null, null);
+INSERT INTO `products` VALUES ('9', 'Áo sơ mi nữ sọc paris', 'êm, thoáng mát', 'hàng tốt, uy tín', '230000', '1', '3', '300000', null, null, null, null);
+INSERT INTO `products` VALUES ('10', 'Áo sơ mi hoạ tiết xích bích', 'Lụa lanh siêu mát,chất rất đẹp ', 'SALE: 195k ( có số lượng ) \r\n- Lụa lanh siêu mát,chất rất đẹp \r\n- Free size: Dưới 53kg\r\n- Màu: Đen, trắng', '195000', '1', '2', '250000', null, null, null, null);
+INSERT INTO `products` VALUES ('11', 'Áo thun thêu hoa hồng và chữ', 'Thun cotton 100%,dày dặn rất đẹp,sờ chất thích liền mặc mát lạnh,from chuẩn đẹp ', '- Chất liệu: Thun cotton 100%,dày dặn rất đẹp,sờ chất thích liền mặc mát lạnh,from chuẩn đẹp \r\n- Màu sắc: Trắng,đen\r\n- Free size: Dưới 55kg', '290000', '1', '3', '350000', null, null, null, null);
+INSERT INTO `products` VALUES ('12', 'Áo thun from rộng lông công', 'Thun cotton 100%,chất vải dày dặn,mịn đẹp,sờ chất thích liền,mặc mát thấm mồ hôi tốt,from rất đẹp ', '- Chất vải: Thun cotton 100%,chất vải dày dặn,mịn đẹp,sờ chất thích liền,mặc mát thấm mồ hôi tốt,from rất đẹp \r\n- Màu sắc: Đỏ\r\n- Free size: Dưới 55kg', '250000', '1', '2', '300000', null, null, null, null);
+INSERT INTO `products` VALUES ('13', 'Quần jeans lưng thun paris', 'thoải mái êm', 'null', '320000', '1', '5', '400000', null, null, null, null);
+INSERT INTO `products` VALUES ('14', 'Quần jean nữ Lucky Brand Lolita Skinny Jean', 'thoải mái, êm', 'Xuất xứ:	Chính hãng nhập từ Mỹ\r\nSize:	Xem bảng hướng dẫn chọn Size\r\nMàu sắc:	Như hình', '3000000', '1', '5', '4000000', null, null, null, null);
+INSERT INTO `products` VALUES ('15', 'Đầm body đan dây sexy', 'Chất thun Nhập Lazza dày dặn', '- Chất thun Nhập Lazza dày dặn \r\n- Freesize 45-54kg cao 1,5-1,65m\r\n- Màu: Xanh rêu, hồng, đen', '360000', '1', '3', '450000', null, null, null, null);
+INSERT INTO `products` VALUES ('16', ' \r\nĐầm body nịch eo kết nút nấm', 'Quý cô công sở quá là sang chảnh', '- Màu sắc: Đen, Đỏ Đô, Hồng dâu, Vàng bò,  Da tây. \r\n- Chất liệu: Thun cotton mỹ . \r\n- Quy cách may: Đường may sườn 1cm. ', '440000', '1', '3', '500000', null, null, null, null);
+INSERT INTO `products` VALUES ('17', 'Giày nam Columbia Granite Ridge Hiking Shoe', 'thoải mái, êm chân', 'Chất liệu:	Da và Vải\r\nĐế giày:	Cao su\r\nChiều cao gót:	2cm\r\nXuất xứ:	Chính hãng nhập từ Mỹ', '2180000', '2', '5', '3000000', null, null, null, null);
+INSERT INTO `products` VALUES ('18', 'Gìay bata viền nâu', 'thoải mái, êm chân', 'Size  35/39', '460000', '2', '3', '500000', null, null, null, null);
+INSERT INTO `products` VALUES ('19', 'Giày nam Nunn Bush Alec Moc Toe', 'nhẹ, thoải mái, êm châm', 'Chất liệu:	Da/Sợi tồng hợp\r\nĐế giày:	Cao su\r\nChiều cao gót:	3cm\r\nXuất xứ:	Chính hãng nhập từ Mỹ', '2730000', '2', '6', '3000000', null, null, null, null);
+INSERT INTO `products` VALUES ('20', 'điện thoại Nokia 6300', 'Các sản phẩm đều là hàng tồn kho của nokia sx tại phần lan', '- Bảo hành 6 tháng (1 đổi 1 trong tuần đầu). \r\n- Các sản phẩm đều là hàng tồn kho của nokia sx tại phần lan. \r\n- Main zin 100% nếu sai tặng luôn 5 máy, bao test imei cho anh em cửa hàng lấy về bán khách lẻ. ', '415000', '3', '20', '500000', null, null, null, null);
+INSERT INTO `products` VALUES ('21', 'điện thoại Iphone 5 16G trắng Unlock', 'Sản phẩm đã qua sử dụng nhưng còn rất mới 98%', 'Chức năng máy còn đầy đủ và làm việc 100%, máy chưa hề qua sửa chữa nhé, sản phẩm xách tay từ USA đã xài mạng AT&T hiện đã unlock', '5690000', '3', '1', '6000000', null, null, null, null);
+INSERT INTO `products` VALUES ('22', 'điện thoại Iphone 5 Black 16Gb unlock', 'Sản phẩm đã qua sử dụng nhưng còn rất mới 98%', 'Chức năng máy còn đầy đủ và làm việc 100%, máy chưa hề qua sửa chữa nhé, sản phẩm xách tay từ USA đã xài mạng AT&T hiện đã unlock', '6190000', '3', '1', '7000000', null, null, null, null);
+INSERT INTO `products` VALUES ('23', ' \r\nLaptop Hp 2560p core i7 2620M', 'Dòng máy doanh nhân cao cấp, vỏ nhôm màu bạc sang trọng', 'Dòng máy doanh nhân cao cấp, vỏ nhôm màu bạc sang trọng. Hàng ship Mỹ nguyên ZIN 100%, \r\nngoại hình mới đẹp 98-99%.', '5000000', '4', '2', '6000000', null, null, null, null);
+INSERT INTO `products` VALUES ('24', 'Laptop Hp 2560p core i5 2520M', 'Dòng máy doanh nhân cao cấp, vỏ nhôm màu bạc sang trọng', 'Dòng máy doanh nhân cao cấp, vỏ nhôm màu bạc sang trọng. Hàng ship Mỹ nguyên ZIN 100%,\r\nngoại hình mới đẹp 98-99%.', '4000000', '4', '2', '5000000', null, null, null, null);
+INSERT INTO `products` VALUES ('25', ' \r\nHP 8560p Core i7 2620M', 'Laptop xách tay USA, còn mới 99%, nguyên zin 100%', 'Laptop xách tay USA, còn mới 99%, nguyên zin 100%', '8500000', '4', '3', '9000000', null, null, null, null);
+INSERT INTO `products` VALUES ('26', ' \r\nĐồng hồ thông minh DZ09', 'Đồng thời đồng hồ thông minh sẽ giúp bạn giảm stress', 'martwatch DZ09 được thế kế bởi màn hình cảm ứng điện dung, có độ nhạy cao. Kích thước 1.55 inch phù hợp với tầm nhìn, độ phân giải màu sắc 240 x 240 meagpixel. Khung viền được gia cố rất kĩ bằng thép không gỉ, dây đeo bằng silicon vừa dẻo dai, vừa bền', '255000', '5', '5', '300000', null, null, null, null);
+INSERT INTO `products` VALUES ('27', 'Đồng hồ nữ Seiko SUT249 Solar Analog Display', 'Mới Full box - nhập khẩu từ Mỹ', 'Bảo hành 24 tháng\r\nGiao hàng miễn phí trên toàn quốc', '3220000', '5', '3', '4000000', null, null, null, null);
+INSERT INTO `products` VALUES ('28', ' \r\nThe Platinum - Đen - VƯƠNG QUỐC ANH', 'sang trọng, quý phái, đẳng cấp', 'Band Loại Chất liệu: Da \r\nTrường hợp Chất liệu: hợp kim \r\nTrường hợp Độ dày: 7 mm \r\nSố mẫu: thạch anh', '1200000', '5', '6', '2000000', null, null, null, null);
+INSERT INTO `products` VALUES ('29', 'Quạt hơi nước PGT-4000G', 'Khi sử dụng máy làm mát USAircooler bạn sẽ không còn phải lo lắng về tiền điện tăng cao', 'công ty chúng tôi dám đảm bảo sản phẩm sẽ tiết kiệm tới 80% điện năng tiêu thụ so với máy lạnh có cùng diện tích sử dụng bên cạnh đó sản phẩm còn ﻿làm sạch không khí giúp bảo vệ sức khỏe cho mọi người đặc biệt là người già và trẻ nhỏ', '8752000', '6', '10', '9500000', null, null, null, null);
+INSERT INTO `products` VALUES ('30', 'MÁY LỌC NƯỚC COWAY CHP-06ER', ' loại và loại bỏ hoàn toàn kim loại nặng', 'MÁY LỌC NƯỚC COWAY CHP-06ER, loại và loại bỏ hoàn toàn kim loại nặng, Clo, Asen, VOC, thuốc tẩy rửa, chất gây ung thư, và khi khuẩn gây hại cho nguồn nước... mang lại chất lượng nước tinh khiết nhất.', '15725000', '6', '5', '16500000', null, null, null, null);
+
+-- ----------------------------
+-- Table structure for sessions
+-- ----------------------------
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) unsigned NOT NULL,
+  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sessions
+-- ----------------------------
+INSERT INTO `sessions` VALUES ('e0ftynbhG_JGItsWSW6XX1ZcJO40p3x7', '1498274432', 0x7B22636F6F6B6965223A7B226F726967696E616C4D6178416765223A6E756C6C2C2265787069726573223A6E756C6C2C22687474704F6E6C79223A747275652C2270617468223A222F227D2C2269734C6F67676564223A66616C73657D);
+INSERT INTO `sessions` VALUES ('qH-Ek0lajmiT9whT_TL-JiRbRbTuwfcf', '1498214907', 0x7B22636F6F6B6965223A7B226F726967696E616C4D6178416765223A6E756C6C2C2265787069726573223A6E756C6C2C22687474704F6E6C79223A747275652C2270617468223A222F227D2C2269734C6F67676564223A66616C73657D);
+INSERT INTO `sessions` VALUES ('vrbBccZB5OSrg-f0NzH0FDw1GfFA4ln1', '1498236001', 0x7B22636F6F6B6965223A7B226F726967696E616C4D6178416765223A6E756C6C2C2265787069726573223A6E756C6C2C22687474704F6E6C79223A747275652C2270617468223A222F227D2C2269734C6F67676564223A66616C73657D);
 
 -- ----------------------------
 -- Table structure for users
@@ -99,3 +169,7 @@ CREATE TABLE `users` (
   `Address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
