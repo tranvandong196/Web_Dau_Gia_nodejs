b75c2696fae6ce69627b06e5207b33e8d40426ba
tranvandong196 <Superhihiha@gmail.com>
2017-06-27 12:11:34 +0700

    Giao diện công điểm - nhận xét
    Giao diện công điểm - nhận xét cho người bán (Site giỏ hàng)

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -685,7 +685,7 @@ productRoute.get('/byFavorite', function(req, res) {
         for (var i = 0; i < data.list.length; i++) {
             var temp = {
                 product: data.list[i],
-                isInFavoriteSite: true,
+                canRemoveProduct: true
             }
             box.push(temp);
         }
@@ -694,7 +694,7 @@ productRoute.get('/byFavorite', function(req, res) {
             box: box,
             isEmpty: data.total === 0,
             catId: req.params.id,
-            Tile: 'Sản phẩm yêu thích (' + data.list.length + ')',
+            Tile: 'Sản phẩm yêu thích',
             pages: pages,
             curPage: curPage,
             prevPage: curPage - 1,
@@ -731,7 +731,6 @@ productRoute.get('/byAuction', function(req, res) {
         for (var i = 0; i < data.list.length; i++) {
             var temp = {
                 product: data.list[i],
-                isInFavoriteSite: false,
             }
             box.push(temp);
         }
@@ -740,7 +739,7 @@ productRoute.get('/byAuction', function(req, res) {
             box: box,
             isEmpty: data.total === 0,
             catId: req.params.id,
-            Tile: 'Sản phẩm đang đấu giá ('+ data.list.length + ')',
+            Tile: 'Sản phẩm đang đấu giá',
             pages: pages,
             curPage: curPage,
             prevPage: curPage - 1,
@@ -776,7 +775,7 @@ productRoute.get('/byBasket', function(req, res) {
         for (var i = 0; i < data.list.length; i++) {
             var temp = {
                 product: data.list[i],
-                isInFavoriteSite: false,
+                canGiveScore_comment: true
             }
             box.push(temp);
         }
@@ -785,7 +784,52 @@ productRoute.get('/byBasket', function(req, res) {
             box: box,
             isEmpty: data.total === 0,
             catId: req.params.id,
-            Tile: 'Sản phẩm thắng đấu giá - giỏ hàng (' + data.list.length + ')',
+            Tile: 'Sản phẩm thắng đấu giá - giỏ hàng',
+            pages: pages,
+            curPage: curPage,
+            prevPage: curPage - 1,
+            nextPage: curPage + 1,
+            showPrevPage: curPage > 1,
+            showNextPage: curPage < number_of_pages - 1,
+        });
+    });
+});
+
+productRoute.get('/byOnSale', function(req, res) {
+
+    var rec_per_page = 6;
+    var curPage = req.query.page ? req.query.page : 1;
+    var offset = (curPage - 1) * rec_per_page;
+    var userIDcurrent = -1;
+    if (res.locals.layoutModels.isLogged)
+        userIDcurrent = res.locals.layoutModels.curUser.id;
+    product.loadPageByBasket(userIDcurrent, rec_per_page, offset).then(function(data) {
+        console.log("[ProductRoute] Da lay danh sach SP dang ban: SoLuong = " + data.list.length)
+        var number_of_pages = data.total / rec_per_page;
+        if (data.total % rec_per_page > 0) {
+            number_of_pages++;
+        }
+
+        var pages = [];
+        for (var i = 1; i <= number_of_pages; i++) {
+            pages.push({
+                pageValue: i,
+                isActive: i === +curPage
+            });
+        }
+        var box = [];   
+        for (var i = 0; i < data.list.length; i++) {
+            var temp = {
+                product: data.list[i]
+            }
+            box.push(temp);
+        }
+        res.render('product/byUser', {
+            layoutModels: res.locals.layoutModels,
+            box: box,
+            isEmpty: data.total === 0,
+            catId: req.params.id,
+            Tile: 'Sản phẩm đang bán',
             pages: pages,
             curPage: curPage,
             prevPage: curPage - 1,
